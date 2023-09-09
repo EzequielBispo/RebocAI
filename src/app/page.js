d07bffe1099051b49b405ec32c82152449016936
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-09 12:36:43 -0300

    Texto adicionado.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
-import Navegacao from './components/Navbar'
-import Titulo from './components/Titulo'
+import Navegacao from "./components/Navbar";
+import Titulo from "./components/Titulo";
+import Subtitulo from "./components/Subtitulo";
+import Texto from "./components/Texto";
 
 export default function Home() {
   return (
     <main>
       <Navegacao />
-      <Titulo name='Abrir ordem de serviço' />
+      <Titulo name="Abrir ordem de serviço" />
+      <Subtitulo name="Informações do Veículo" />
+      <Texto name="Placa do caminhão:" />
+      {/* <Texto name="Altura do veículo(metros):" />
+      <Texto name="Tipo de Carga:" />
+      <Texto name="Peso da carga(kg):" /> */}
     </main>
-  )
+  );
 }
