d0e74d867f312a93bd7f3e0ccb96fbc623e961ef
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-09 12:18:16 -0300

    Titulo edicionado.

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Botao from './components/Botao'
 import Navegacao from './components/Navbar'
+import Titulo from './components/Titulo'
 
 export default function Home() {
   return (
     <main>
       <Navegacao />
+      <Titulo name='Abrir ordem de serviço' />
     </main>
   )
 }
