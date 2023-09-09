ee5a0f822ef63694baafb09ea6b0c786a78ba711
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-08 22:27:56 -0300

    Navbar pronta.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Botao from './components/Botao'
+import Navegacao from './components/Navbar'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <Botao name='solicitar guincho'/>
+    <main>
+      <Navegacao />
     </main>
   )
 }
