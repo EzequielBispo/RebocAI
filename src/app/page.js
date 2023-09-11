3e789e5ea1391dbd6c1f96784efa4766d1d1bba9
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 01:25:04 -0300

    Correção organizacional.

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,9 @@
-import Navegacao from "./components/Navbar";
-import Titulo from "./components/Titulo";
-import Subtitulo from "./components/Subtitulo";
-import Texto from "./components/Texto";
 import CampoCinza from "./components/CampoCinza";
 
 
 export default function Home() {
   return (
     <main>
-      <Navegacao />
-      <Titulo name="Abrir ordem de serviço" />
-      <Subtitulo name="Informações do Veículo" />
-      <Texto name="Placa do caminhão:" />
-      {/* <Texto name="Altura do veículo(metros):" />
-      <Texto name="Tipo de Carga:" />
-      <Texto name="Peso da carga(kg):" /> */}
-      <CampoCinza />
-      <CampoCinza />
-      <CampoCinza />
     </main>
   );
 }
