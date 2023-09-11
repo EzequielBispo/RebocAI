482a07731b94915e66bdfa33db39175b84c8f065
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 01:48:36 -0300

    Correção de CSS.

--- src/app/components/Texto.js
+++ src/app/components/Texto.js
@@ -1,22 +1,19 @@
-"use client"
+"use client";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Texto = styled.p`
-position: absolute;
-width: 202px;
-height: 24px;
-font-style: normal;
-font-weight: 400;
-font-size: 20px;
-line-height: 24px;
-color: #000000;
-`
+  width: 202px;
+  height: 24px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+  color: #000000;
+`;
 
-function FunTexto(props){
-    return(
-        <Texto>{props.name}</Texto>
-    )
+function FunTexto(props) {
+  return <Texto>{props.name}</Texto>;
 }
 
 export default FunTexto;
