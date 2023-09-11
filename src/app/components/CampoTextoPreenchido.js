da2219472e378845e0d54cc4f2bdfc701dbde681
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 00:47:02 -0300

    Placeholder criado.

---
+++ src/app/components/CampoTextoPreenchido.js
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+const CampoTextoPreenchido = styled.input``;
+
+function FunCampoTextoPreenchido(props) {
+  return (
+    <>
+      <CampoTextoPreenchido placeholder={props.name}></CampoTextoPreenchido>
+    </>
+  );
+}
+
+export default FunCampoTextoPreenchido;
