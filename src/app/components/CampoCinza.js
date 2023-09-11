52622c45d2097bd203276ddf2c2526b352e985f6
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 00:11:54 -0300

    Campo Cinza sem posição.

--- src/app/components/CampoCinza.js
+++ src/app/components/CampoCinza.js
@@ -1,24 +1,16 @@
-"use client"
+"use client";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const CampoCinza = styled.div`
+  width: 160px;
+  height: 28px;
+  background: #e2e2e2;
+  border-radius: 4px;
+`;
 
-/* Rectangle 20 */
-
-position: absolute;
-width: 160px;
-height: 28px;
-left: 227px;
-top: 262px;
-background: #E2E2E2;
-border-radius: 4px;
-`
-
-function FunCampoCinza(props){
-    return(
-        <CampoCinza />
-    )
+function FunCampoCinza(props) {
+  return <CampoCinza />;
 }
 
 export default FunCampoCinza;
