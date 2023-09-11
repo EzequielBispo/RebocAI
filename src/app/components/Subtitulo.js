2df6283ebe62989c20f7b7fd8316d43884e80d64
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 01:29:47 -0300

    Estilo do Subtitulo alterado.

--- src/app/components/Subtitulo.js
+++ src/app/components/Subtitulo.js
@@ -3,19 +3,12 @@
 import styled from "styled-components";
 
 const Subtitulo = styled.h2`
-  /* Informações do Veículo */
-
-  position: absolute;
   width: 243px;
   height: 24px;
-  left: 86px;
-  top: 203px;
-
   font-style: normal;
   font-weight: 600;
   font-size: 20px;
   line-height: 24px;
-
   color: #000000;
 `;
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import Navegacao from './components/Navbar'
-import Titulo from './components/Titulo'
 import Subtitulo from './components/Subtitulo'
 
 export default function Home() {
   return (
     <main>
-      <Navegacao />
-      <Titulo name='Abrir ordem de serviço' />
       <Subtitulo name='Informações do veículo' />
     </main>
   )
