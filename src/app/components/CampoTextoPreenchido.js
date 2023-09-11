90d87ea0b406b4e140bba52d6ddfa72a95f34654
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-11 20:54:25 -0300

    Personalização do primeiro component referenciado.

--- src/app/components/CampoTextoPreenchido.js
+++ src/app/components/CampoTextoPreenchido.js
@@ -3,12 +3,18 @@
 import styled from "styled-components";
 
 const CampoTextoPreenchido = styled.input`
-width: 93px;
-height: 15px;
-color: #A1A5A9;
-border: 2px solid #A1A5A9;
-border-style: none none solid none;; 
+  width: 93px;
+  height: 15px;
+  color: #a1a5a9;
+  border: 2px solid #a1a5a9;
+  border-style: none none solid none;
 
+  ${(props) =>
+    props.first &&
+    `
+    width: 200px;
+
+  `}
 `;
 
 function FunCampoTextoPreenchido(props) {
--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,10 @@ import CampoTextoPreenchido from './components/CampoTextoPreenchido'
 export default function Home() {
   return (
     <main>
+      <CampoTextoPreenchido></CampoTextoPreenchido>
+      <CampoTextoPreenchido></CampoTextoPreenchido>
+      <CampoTextoPreenchido></CampoTextoPreenchido>
+      <CampoTextoPreenchido></CampoTextoPreenchido>
 
     </main>
   )
