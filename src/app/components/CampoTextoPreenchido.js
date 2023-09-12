6988850cc82002dcbb189d9cf549ee1efe4772ce
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-12 15:15:57 -0300

    Margem abaixo adicionada.

--- src/app/components/CampoTextoPreenchido.js
+++ src/app/components/CampoTextoPreenchido.js
@@ -3,11 +3,11 @@
 import styled from "styled-components";
 
 const CampoTextoPreenchido = styled.input`
+margin-bottom: 10px;
 height: 15px;
 color: #A1A5A9;
 border: 2px solid #A1A5A9;
 border-style: none none solid none;; 
-
 `;
 
 function FunCampoTextoPreenchido(props) {
