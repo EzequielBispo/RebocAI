a8ce7362a050ef74d9f0fd5e30577ff9960573ed
Ezequiel Bispo <ezequielbispo011@gmail.com>
2023-09-12 14:48:37 -0300

    Margem a esquerda diminuida.

--- src/app/components/CampoCinza.js
+++ src/app/components/CampoCinza.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const CampoCinza = styled.input`
   width: 160px;
   height: 28px;
-  margin-left: 10px;
+  margin-left: 3px;
   background: #e2e2e2;
   border-radius: 4px;
 `;
